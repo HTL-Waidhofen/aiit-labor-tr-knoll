6855bfb4fdc1ceb802c03b6538de2779efe1b739
tr-knoll <tristan.knoll@htlwy.at>
2025-12-12 12:40:39 +0100

    added visual studio project with no code

---
+++ Klassen und Objekte/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassen_und_Objekte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
