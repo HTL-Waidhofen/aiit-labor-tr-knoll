09605292e67a3da90ab83573aca130f262b26582
tr-knoll <tristan.knoll@htlwy.at>
2026-01-09 11:34:37 +0100

    New Project Quader

---
+++ Lab03_Quadar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_Quadar
+{
+    class Quader
+    {
+        private double hoehe;
+        private double breite;
+        private double laenge;
+
+        public Quader() : this(1, 1, 1)
+        {
+            this.hoehe = 1;
+            this.breite = 1;
+            this.laenge = 1;
+        }
+
+        public Quader(double hoehe, double breite, double laenge)
+        {
+            this.hoehe = hoehe;
+            this.breite = breite;
+            this.laenge = laenge;
+        }
+       
+
+    
+    
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quader q1 = new Quader(3,2,1);
+            Quader q2 = new Quader();
+
+        }
+    }
+}
